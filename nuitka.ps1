24d8e8af945e75076be90d74242ebe4d21e14b9b
山泥若叶睦 <102826168@qq.com>
2026-04-29 16:18:30 +0800

    将 nuitka.ps1 文件添加到 .gitignore 中，并删除该文件

--- nuitka.ps1
+++
@@ -1,9 +0,0 @@
-$python = ".\.venv\Scripts\python.exe"
-
-& $python -m nuitka `
-	--onefile `
-	--windows-console-mode=disable `
-	--output-dir=output/nuitka `
-    --jobs=12 `
-	--enable-plugin=pyside6 `
-	src/main.py
